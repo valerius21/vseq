122ef1b6866460aaaca15b389ce1dc83447a1b04
Valerius <mail@valerius.me>
2022-09-27 12:06:47 +0200

    Update index.ts
    ad jsdoc

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,9 @@ import range from "lodash.range";
 
 /**
  * https://lodash.com/docs/4.17.15#range with the exception, that the end-number is inclusive.
+ * @param {number} start - the number to start on
+ * @param {number} end - the number to end on, which is inclusive
+ * @param {number} step - the stepsize the sequence is based on
+ * @return {number[]} the resulting number sequence as an array
  */
 export const seq = (start: number, end?: number, step?: number) => range(start, end ? end + 1 : end, step);
